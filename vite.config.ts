2479f0438b64fd1ab44e60f600c04e99ef622670
fustates <fustates@gmail.com>
2025-08-12 23:07:30 +0800

    build(vite): 添加 gzip 压缩功能
    - 在 package.json 中添加 vite-plugin-compression 依赖
    - 在 vite.config.ts 中配置 viteCompression 插件
    - 插件配置包括文件过滤、阈值、算法、后缀名和是否删除源文件等选项

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteCompression({
+      filter: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i, // 需要压缩的文件
+      threshold: 1024, // 文件容量大于这个值进行压缩
+      algorithm: "gzip", // 压缩方式
+      ext: "gz", // 后缀名
+      deleteOriginFile: false, // 压缩后是否删除压缩源文件
+    }),
+  ],
 });
