5a7877ad2e29262efd5b32379e3a1950a5f3ee75
fustates <fustates@gmail.com>
2025-08-30 18:28:28 +0800

    style(view): 优化关于页面和 404 页面的代码结构
    - 调整了关于页面的 App 组件结构，简化了代码格式
    - 优化了 404 页面的 HTML 结构，移除了多余的空格和换行

--- src/view/about/index.tsx
+++ src/view/about/index.tsx
@@ -1,16 +1,13 @@
 function App() {
-
-    return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                </a>
-                <a href="https://react.dev" target="_blank">
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank"></a>
+        <a href="https://react.dev" target="_blank"></a>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/view/error/404.tsx
+++ src/view/error/404.tsx
@@ -5,9 +5,7 @@ const NotFound: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] bg-white rounded-lg shadow-lg p-8 text-center">
       <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
-      <h2 className="text-3xl font-semibold text-gray-800 mb-6">
-        页面未找到
-      </h2>
+      <h2 className="text-3xl font-semibold text-gray-800 mb-6">页面未找到</h2>
       <p className="text-lg text-gray-600 mb-8 max-w-md">
         抱歉，您访问的页面不存在或已被删除。请检查URL是否正确，或返回首页继续浏览。
       </p>
