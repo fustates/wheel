673493f98875244183368bc93df3f9ccbd0fd7d5
fustates <fustates@gmail.com>
2026-03-15 22:23:19 +0800

    chore: 将压缩算法更新为Brotli，并在工作流中添加Docker构建和推送步骤

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
     viteCompression({
       filter: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i, // 需要压缩的文件
       threshold: 1024, // 文件容量大于这个值进行压缩
-      algorithm: "gzip", // 压缩方式
-      ext: "gz", // 后缀名
+      algorithm: "brotliCompress", // 压缩方式
+      ext: "br", // 后缀名
       deleteOriginFile: false, // 压缩后是否删除压缩源文件
     }),
   ],
